db2d614bd8eb8b07c0fe0d1c04f7bdb97a83123d
Chandrakant <83269908+nightfury000@users.noreply.github.com>
2026-04-03 01:39:29 +0530

    created a simple vpc setup having a public subnet, private subnet, igw, rtb, routes, rtb-association

---
+++ vpc/vpc.tf
@@ -0,0 +1,79 @@
+#version block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+# create vpc
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
+  
+}
+
+
+
+### PUBLIC SUBNET ###
+# create public subnet
+resource "aws_subnet" "my-public-subnet" {
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "my-public-subnet"
+  }
+}
+
+# create internet gateway
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+
+# create route table
+resource "aws_route_table" "my-route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+# create route to internet gateway
+resource "aws_route" "my-route" {
+  route_table_id = aws_route_table.my-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my-igw.id
+}
+
+# associate route table with public subnet
+resource "aws_route_table_association" "my-route-table-association" {
+  subnet_id = aws_subnet.my-public-subnet.id
+  route_table_id = aws_route_table.my-route-table.id
+} 
+
+
+
+## PRIVATE SUBNET ###
+# create private subnet
+resource "aws_subnet" "my-private-subnet" {
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "my-private-subnet"
+  }
+} 
+
